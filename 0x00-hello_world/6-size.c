dc6bf5c613afafea267434046aafc86968158ac9
absyinka <absyinka@gmail.com>
2022-04-07 10:38:39 -0700

    Type error fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of a char: %lu bytes\n", sizeof(char));
 	printf("Size of a int: %lu bytes\n", sizeof(int));
 	printf("Size of a long int: %lu bytes\n", sizeof(long));
-	printf("Size of a long long int: %lu bytes\n", sizeof(long double));
+	printf("Size of a long long int: %lu bytes\n", sizeof(long long));
 	printf("Size of a float: %lu bytes\n", sizeof(float));
 
 	return (0);
