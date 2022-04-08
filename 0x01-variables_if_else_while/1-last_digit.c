7c4e3798492d681dd163c223a087499a39bebf01
absyinka <absyinka@gmail.com>
2022-04-08 07:16:49 -0700

    Task 1: checking the length of random number generated to output messages

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int length;
+	char text[] = "Last digit of";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	length = n % 10;
+
+	if (length > 5)
+	{
+		printf("%s %d is %d and is greater than 5\n", text, n, length);
+	}
+	else if (length == 0)
+	{
+		printf("%s %d is %d and is 0\n", txt, n, length);
+	}
+	else if (length < 6)
+	{
+		printf("%s %d is %d and is less than 6 and not 0\n", txt, n, length);
+	}
+
+	return (0);
+}
