d130b82242a19bf65da038feaf4d7aca3808c264
absyinka <absyinka@gmail.com>
2022-04-08 13:45:59 -0700

    Task 8: script to print all base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char cchar;
+
+	for (num = '0'; num <= '9' num++)
+		putchar(num);
+	for (cchar = 'a'; cchar <= 'f'; cchar++)
+		putchar(cchar);
+	putchar('\n');
+
+	return (0);
+}
