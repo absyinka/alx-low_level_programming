6f058f07818d1906b9d9297e17023175261164c6
absyinka <absyinka@gmail.com>
2022-04-12 21:55:46 +0100

    Task 10: function to add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * add - add two integers
+ * @n1: first integer
+ * @n2: second integer
+ * Return: sum of n1 and n2
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
