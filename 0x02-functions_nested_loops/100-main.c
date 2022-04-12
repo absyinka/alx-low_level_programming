42e368e427e03c260529c67f0e3ba62bd2b02860
absyinka <absyinka@gmail.com>
2022-04-12 22:15:28 +0100

    Task 11: print number to 98

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -8,9 +8,11 @@
  */
 int main(void)
 {
-    int n;
+    print_to_98(0);
+    print_to_98(98);
+    print_to_98(111);
+    print_to_98(81);
+    print_to_98(-10);
 
-    n = add(89, 9);
-    printf("%d\n", n);
     return (0);
 }
