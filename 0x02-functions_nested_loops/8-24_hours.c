552bc781fbacb22d2ed1311ce631cb39877831e1
absyinka <absyinka@gmail.com>
2022-04-12 21:09:37 +0100

    Task 8: print all the time in Jack Bauer 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * jack_bauer - display all the time of Jack in 24hours
+ *
+ * Return: null
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
