29ceaa070d1212b428da3d9e25ec64c0d736fe07
absyinka <absyinka@gmail.com>
2022-04-12 21:47:36 +0100

    Task 9: function build 9ce multiplication table starting from 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * times_table - 9ce multiplication table
+ *
+ * Return: result of the multiplication table
+ */
+void times_table(void)
+{
+	int num, num1;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (num1 = 1; num1 <= 9; num1++)
+		{
+			int result;
+
+			result = (num * num1);
+			if ((result / 10) > 0)
+			{
+				_putchar((result / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((result % 10) + '0');
+			if (num1 < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
