04fb892bed91dad430c3f2807d291b528ad29167
absyinka <absyinka@gmail.com>
2022-04-15 00:20:10 +0100

    Task 12: function to print any integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - print integer value
+ * @n: integer to be printed
+ * Return: null
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+	if (num / 10 != 0)
+	{
+		print_number(num / 10);
+		_putchar((num % 10) + '0');
+	}
+}
