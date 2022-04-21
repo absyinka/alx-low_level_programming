0bd6009e0723202ca5c0f698c369afab9e16d0c7
absyinka <absyinka@gmail.com>
2022-04-21 01:48:08 -0700

    Task 0: updates the value of a number to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  *reset_to_98 - Takesa pointer to an integer to updated the value to 98
+  *@n: The pointer o an int
+  *Return: null
+  */
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#include<studio.h>
+
+char _putchar(char c);
+void reset_to_98(int *n);
