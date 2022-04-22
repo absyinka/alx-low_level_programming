0df2e2a69bd458929c1bb8b73d4e4118384fe2dc
absyinka <absyinka@gmail.com>
2022-04-22 09:04:55 -0700

    Task 5: function to change all lowercase letter to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+  * string_toupper - change all characters in a string to uppercase
+  * @n: character in the string
+  * Return: @n
+  */
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] -= 32;
+		}
+		i++;
+	}
+	return (n);
+}
