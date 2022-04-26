d7842249e889869e3faa9edf7a53d560915913cb
absyinka <absyinka@gmail.com>
2022-04-26 07:49:45 -0700

    Task 8: not completed yet

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,3 +1,12 @@
 #include "main.h"
 
+/**
+  * set_string - set the value of a point to char
+  * @s: double pointer param
+  * @to: pointer to set value to
+  * Return void
+  */
+void set_string(char **s, char *to)
+{
 
+}
