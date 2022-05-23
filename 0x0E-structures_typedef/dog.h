f8830f3d650da3ced642d40e36b7009eec56be8e
absyinka <absyinka@gmail.com>
2022-05-23 04:48:25 -0700

    Task 0: creating an header file that contains new struct of dog type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A new type of a dog
+ * @name: The dog name
+ * @age: The dog age
+ * @owner: The dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
