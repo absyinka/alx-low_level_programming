642459e0c7f68cc3604e323d3ba1eb9ac112cade
absyinka <absyinka@gmail.com>
2022-05-27 20:09:47 -0700

    Task 5: functions that prints string before the main function executes

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * start_before_main - prints a strings before main function
+ * gets executed
+ */
+
+void start_before_main(void)
+{
+	printf("You're beat! and yet, you must ");
+	printf("allow,\nI bore my house upon my back!\n");
+}
